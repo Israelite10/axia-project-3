d6ad0b33395f10c396863458daa738ef6337a691
Bello Israel <belloisrael1999@gmail.com>
2025-03-28 13:32:37 -0700

    .

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/axia-project-3/',
+
   plugins: [
     react(),
     tailwindcss(),
